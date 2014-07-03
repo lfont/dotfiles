779e209864630a491c034e3c2df59f36e84bc3b1
Loïc Fontaine <ljph.fontaine@gmail.com>
2014-07-03 15:34:51 +0200

    Fish cannot be installed via apt on NixOS. Vim's install.fish should link the configuration.

--- fish/install.sh
+++ fish/install.sh
@@ -8,7 +8,8 @@ then
         brew install fish
 
         echo $(which fish) >> /etc/shells
-    else
+    elif test ! $(which nixos-install)
+    then
         sudo apt-add-repository ppa:fish-shell/release-2
         sudo apt-get update
         sudo apt-get install fish
--- vim/install.fish
+++ vim/install.fish
@@ -1,3 +1,7 @@
+ln -sf (pwd)/vim/gvimrc.local        ~/.gvimrc.local
+ln -sf (pwd)/vim/vimrc.bundles.local ~/.vimrc.bundles.local
+ln -sf (pwd)/vim/vimrc.local         ~/.vimrc.local
+
 fish ./vim/install-spf13.fish
 
 exit 0
