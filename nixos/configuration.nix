96406ed0929795228287ccb9b57d198f0ff30140
Loïc Fontaine <ljph.fontaine@gmail.com>
2014-07-04 21:53:48 +0200

    Improving the configuration for the macbook.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,19 +10,24 @@
       ./hardware-configuration.nix
     ];
 
+  hardware.pulseaudio.enable = true;
+
   # Use the gummiboot efi boot loader.
-  boot.loader.grub.enable = false;
-  boot.loader.grub.device = "/dev/sda4";
   boot.loader.gummiboot.enable = true;
   boot.loader.gummiboot.timeout = 4;
   boot.loader.efi.canTouchEfiVariables = false;
+  boot.extraModprobeConfig = ''
+    options hid_apple fnmode=2
+  '';
+  boot.postBootCommands = ''
+    echo 0 > /sys/class/leds/smc::kbd_backlight/brightness
+  '';
 
   networking.hostName = "loics-macbook-pro"; # Define your hostname.
   networking.networkmanager.enable = true;
-  networking.extraHosts = 
-    ''
-      192.168.0.18 bibimbap
-    ''; 
+  networking.extraHosts = ''
+    192.168.0.18 bibimbap
+  '';
 
   # Select internationalisation properties.
   i18n = {
@@ -46,9 +51,6 @@
     xarchiver
     gnome3.gtk
     gtk-engine-murrine
-    #librsvg
-    #gdk_pixbuf
-    #cairo
 
     vimHugeX
     git
@@ -65,9 +67,15 @@
 
   environment.shells = [
     "/run/current-system/sw/bin/fish"
-  ];	
+  ];
+
+  # There is no way to define better sudo rules in 14.04
+  # This is needed for the keyboard-backlight script
+  security.sudo.wheelNeedsPassword = false;
 
   # List services that you want to enable:
+
+  services.acpid.enable = true;
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
@@ -103,6 +111,22 @@
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.packageOverrides = pkgs: {
+    # smb support
     gvfs = pkgs.gvfs.override { lightWeight = false; };
+    # svg support
+   # xfce.xfce4panel = (pkgs.lib.overrideDerivation pkgs.xfce.xfce4panel (attrs: {
+   #     buildInputs = [
+   #         pkgconfig intltool gtk xfce.libxfce4util xfce.exo libwnck
+   #         xfce.garcon xfce.xfconf libstartup_notification
+   #         gdk_pixbuf librsvg
+   #     ];
+
+   #     preFixup = ''
+   #         wrapProgram "$out/bin/xfce4-panel" \*/
+   #             --set GDK_PIXBUF_MODULE_FILE "$GDK_PIXBUF_MODULE_FILE"
+   #         rm $out/share/icons/hicolor/icon-theme.cache
+   #     '';
+   # }));
   };
 }
+
--- nixos/install.fish
+++ nixos/install.fish
@@ -2,8 +2,12 @@ if not uname -a | grep NixOS
   exit 0
 end
 
+. ./functions/link_command.fish
+
 sudo ln -sf (pwd)/nixos/hardware-configuration.nix /etc/nixos/
 sudo ln -sf (pwd)/nixos/configuration.nix          /etc/nixos/
+
+link_command (pwd)/nixos/keyboard-backlight.sh keyboard-backlight
 
 exit 0
 
