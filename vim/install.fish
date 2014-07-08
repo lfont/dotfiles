78b47713d3ecc4e0fc49e1044bbef7f4fe10045c
Loïc Fontaine <ljph.fontaine@gmail.com>
2014-07-08 15:20:25 +0200

    vimrc.before.local should be use to disable extra spf13 packages.

--- vim/install.fish
+++ vim/install.fish
@@ -1,7 +1,3 @@
-ln -sf (pwd)/vim/gvimrc.local        ~/.gvimrc.local
-ln -sf (pwd)/vim/vimrc.bundles.local ~/.vimrc.bundles.local
-ln -sf (pwd)/vim/vimrc.local         ~/.vimrc.local
-
 fish ./vim/install-spf13.fish
 
 exit 0
