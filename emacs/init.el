50ed7b496074553ffe6147b5a1caf7bce9186a3a
Loïc Fontaine <ljph.fontaine@gmail.com>
2014-07-28 19:54:43 +0200

    Improves emacs's configuration. Disables YouCompleteMe auto completion.

---
+++ emacs/init.el
@@ -0,0 +1,100 @@
+;; https://github.com/mklappstuhl/dotfiles/blob/master/emacs.d/init.el
+;; http://stackoverflow.com/questions/8095715/emacs-auto-complete-mode-at-startup
+
+(require 'package)
+;; list the packages you want
+(setq package-list '(projectile
+                     flx-ido
+                     magit
+                     git-gutter
+                     solarized-theme
+                     zenburn-theme
+                     tern
+                     auto-complete
+                     tern-auto-complete
+                     fill-column-indicator))
+
+;; list the repositories containing them
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; activate all the packages (in particular autoloads)
+(package-initialize)
+
+;; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Always ALWAYS use UTF-8
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(require 'iso-transl)
+
+;; Always ask for y/n keypress instead of typing out 'yes' or 'no'
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+(transient-mark-mode 1) ;; No region when it is not highlighted
+(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
+
+;; no backup
+(setq make-backup-files nil)
+
+;; General UI stuff
+(global-linum-mode t)
+(global-hl-line-mode t)
+(setq default-tab-width 2)
+(setq inhibit-startup-message t)
+(setq visible-bell 'top-bottom)
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(unless (display-graphic-p) (menu-bar-mode -1))
+;(setq x-underline-at-descent-line t)
+;(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+;(setq mouse-wheel-progressive-speed nil)
+;(setq mouse-wheel-follow-mouse 't)
+;(setq scroll-step 1)
+
+(add-hook 'window-configuration-change-hook
+          (lambda ()
+            (set-window-margins (car (get-buffer-window-list (current-buffer) nil t)) 2 2)))
+
+(load-theme 'zenburn t)
+(set-default-font "Inconsolata 11")
+
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+(require 'projectile)
+(projectile-global-mode)
+(setq projectile-enable-caching 0)
+
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
+
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+   '(progn
+      (require 'tern-auto-complete)
+      (tern-ac-setup)))
+
+(require 'git-gutter)
+(global-git-gutter-mode t)
+
+(require 'fill-column-indicator)
+(setq fci-rule-width 3)
+(setq fci-rule-color "grey")
+(add-hook 'prog-mode-hook 'fci-mode)
+
---
+++ emacs/install.fish
@@ -0,0 +1,3 @@
+mkdir -p ~/.emacs.d/
+ln -sf (pwd)/emacs/init.el ~/.emacs.d/
+
