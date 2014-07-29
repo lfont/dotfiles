4b77bf8458c5494e43ae0b0f9b2839cff8a091bb
Loïc Fontaine <ljph.fontaine@gmail.com>
2014-07-29 11:19:32 +0200

    Updates firefox to version 34.

--- firefox/update.fish
+++ firefox/update.fish
@@ -2,7 +2,7 @@ if test ! (which nix-env)
     exit 0
 end
 
-set source_version "33.0a1"
+set source_version "34.0a1"
 set source_url http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-{$source_version}.en-US.linux-x86_64.tar.bz2
 set source_sha256 (curl $source_url | sha256sum | sed 's/  -$//')
 
