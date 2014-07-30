63a980d1b785d953a016a87cf0b20314ba6338cf
Loïc Fontaine <ljph.fontaine@gmail.com>
2014-07-30 18:05:07 +0200

    Updates emacs configuration: Let emacsclient start the server if needed. Adds new packages: smex, flycheck, php-mode. Git-Gutter and linenum does not work well in terminal together (emacs crash), so disables linenum in terminal temporarily. Improves C-a. Implements <backtab>.

--- emacs/e.fish
+++ emacs/e.fish
@@ -1,10 +1,4 @@
 #!/usr/bin/env fish
 
-if begin; test (pidof emacs); or test (pidof emacs24); end
-   emacsclient -nw $argv
-else
-  emacs -nw $argv
-end
-
-exit $status
+exec emacsclient -nw --alternate-editor="" -c $argv
 
--- emacs/init.el
+++ emacs/init.el
@@ -1,22 +1,23 @@
-;; https://github.com/mklappstuhl/dotfiles/blob/master/emacs.d/init.el
-;; http://stackoverflow.com/questions/8095715/emacs-auto-complete-mode-at-startup
-
-(server-start)
+;;; https://github.com/mklappstuhl/dotfiles/blob/master/emacs.d/init.el
+;;; http://stackoverflow.com/questions/8095715/emacs-auto-complete-mode-at-startup
 
 (require 'package)
 ;; list the packages you want
 (setq package-list '(projectile
                      flx-ido
                      magit
-;                     git-gutter
-;                     solarized-theme
+                     git-gutter
                      zenburn-theme
+                     auto-complete
                      tern
                      tern-auto-complete
                      fill-column-indicator
                      multiple-cursors
                      highlight-chars
-                     rainbow-delimiters))
+                     rainbow-delimiters
+                     smex
+                     php-mode
+                     flycheck))
 
 ;; list the repositories containing them
 (add-to-list 'package-archives
@@ -52,12 +53,12 @@
 (setq auto-save-default nil) ; stop creating those #autosave# files
 
 ;; General UI stuff
-(global-linum-mode t)
+(when (display-graphic-p) (global-linum-mode t))
 (global-hl-line-mode t)
 (column-number-mode 1)
 
 ;; Tab behavior
-(add-hook 'text-mode-hook (lambda ()
+(add-hook 'prog-mode-hook (lambda ()
     (setq indent-tabs-mode nil)
     (setq tab-width 4)
     (setq tab-stop-list (number-sequence 4 200 4))
@@ -70,7 +71,8 @@
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (unless (display-graphic-p) (menu-bar-mode -1))
 
-;(setq x-underline-at-descent-line t)
+(setq x-underline-at-descent-line t)
+
 ;(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 ;(setq mouse-wheel-progressive-speed nil)
 ;(setq mouse-wheel-follow-mouse 't)
@@ -81,10 +83,12 @@
 ;            (set-window-margins (car (get-buffer-window-list (current-buffer) nil t)) 2 2)))
 
 (load-theme 'zenburn t)
-(set-default-font "Inconsolata 11")
+(set-frame-font "Inconsolata 11")
 
-(add-hook 'prog-mode-hook 'electric-indent-mode)
-(add-hook 'prog-mode-hook 'electric-pair-mode)
+(add-hook 'prog-mode-hook (lambda ()
+    ;(add-hook 'prog-mode-hook 'electric-indent-mode)
+    (add-hook 'prog-mode-hook 'electric-pair-mode)
+    (add-hook 'prog-mode-hook 'flycheck-mode)))
 
 (require 'auto-complete)
 (global-auto-complete-mode t)
@@ -107,8 +111,13 @@
       (require 'tern-auto-complete)
       (tern-ac-setup)))
 
-;(require 'git-gutter)
-;(global-git-gutter-mode t)
+(require 'git-gutter)
+(global-git-gutter-mode t)
+(when (display-graphic-p) (git-gutter:linum-setup))
+(unless (display-graphic-p)
+    (custom-set-variables
+        '(git-gutter:separator-sign "|"))
+    (set-face-foreground 'git-gutter:separator "grey"))
 
 (require 'fill-column-indicator)
 (setq fci-rule-width 3)
@@ -129,5 +138,54 @@
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
+(require 'smex) ; Not needed if you use package.el
+(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+                  ; when Smex is auto-initialized on its first run.
+(global-set-key (kbd "M-x") 'smex)
+
+;; http://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/
+(defun smarter-move-beginning-of-line (arg)
+    "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+    (interactive "^p")
+    (setq arg (or arg 1))
+
+    ;; Move lines first
+    (when (/= arg 1)
+      (let ((line-move-visual nil))
+        (forward-line (1- arg))))
+
+    (let ((orig-point (point)))
+      (back-to-indentation)
+      (when (= orig-point (point))
+        (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line]
+                                'smarter-move-beginning-of-line)
+
+;; http://stackoverflow.com/questions/23692879/emacs24-backtab-is-undefined-how-to-define-this-shortcut-key
+(defun un-indent-by-removing-4-spaces ()
+  "remove 4 spaces from beginning of of line"
+  (interactive)
+  (save-excursion
+    (save-match-data
+      (beginning-of-line)
+      ;; get rid of tabs at beginning of line
+      (when (looking-at "^\\s-+")
+        (untabify (match-beginning 0) (match-end 0)))
+      (when (looking-at "^    ")
+        (replace-match "")))))
+
+(global-set-key (kbd "<backtab>") 'un-indent-by-removing-4-spaces)
+
 ;; background color bug https://github.com/bbatsov/solarized-emacs/issues/18
 (custom-set-faces (if (not window-system) '(default ((t (:background "nil"))))))
+
