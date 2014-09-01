525f21ae5358dfdd0ef35c0f9444fb23d2462b92
Loïc Fontaine <loicfontaine@fastmail.fm>
2014-09-01 16:39:33 +0200

    Adds the notify.el plugin to the emacs install.

--- emacs/install.fish
+++ emacs/install.fish
@@ -3,6 +3,9 @@
 mkdir -p ~/.emacs.d/
 ln -sf (pwd)/emacs/init.el ~/.emacs.d/
 
+mkdir -p ~/.emacs.d/lisp
+ln -s (pwd)/emacs/notify.el ~/.emacs.d/lisp/
+
 ln -sf (pwd)/emacs/gnus.el ~/.gnus.el
 
 link_command (pwd)/emacs/e.fish e
