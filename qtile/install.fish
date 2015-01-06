fdf703498dfe429b5cf58a22917d0ad24fad33b8
Loïc Fontaine <loicfontaine@fastmail.fm>
2015-01-06 18:10:06 +0100

    Splits the desktop folder in more specific folders.

---
+++ qtile/install.fish
@@ -0,0 +1,23 @@
+. ./functions/link_command.fish
+. ./functions/install_repository.fish
+
+# udev rules
+sudo ln -sf (pwd)/udev/51-android.rules        /etc/udev/rules.d/
+sudo ln -sf (pwd)/udev/100-android-mount.rules /etc/udev/rules.d/
+
+# X
+ln -sf (pwd)/x11/xinitrc        ~/.xinitrc
+ln -sf (pwd)/x11/xprofile       ~/.xprofile
+ln -sf (pwd)/x11/Xresources     ~/.Xresources
+ln -sf (pwd)/x11/qtile-xsession ~/.xsession
+
+# qtile
+mkdir -p ~/.config/qtile
+ln -sf (pwd)/qtile/config.py ~/.config/qtile/
+
+# dmenu
+mkdir -p ~/.config/dmenu
+ln -sf (pwd)/dmenu/dmenu-bind.sh ~/.config/dmenu/
+
+exit 0
+
