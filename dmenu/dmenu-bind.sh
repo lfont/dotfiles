fc1b934a24c5c0427a2a9ec1129bff3e07c40a55
Loïc Fontaine <loicfontaine@fastmail.fm>
2015-01-14 18:30:02 +0100

    [dmenu] Use xft font (works with dmenu2 from nix).

--- dmenu/dmenu-bind.sh
+++ dmenu/dmenu-bind.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-exe=`dmenu_run -l 5 -nb '#151617' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#151617' -fn '-xos4-terminus-medium-r-*-*-14-*'` && eval "exec $exe"
+#!/usr/bin/env bash
+exe=`dmenu_run -l 5 -nb '#151617' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#151617' -fn 'Inconsolata-11:Regular'` && eval "exec $exe"
