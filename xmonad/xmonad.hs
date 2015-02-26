a12704ed0d31a47d5e6847a625bbfb501ae3b5f4
Loïc Fontaine <loicfontaine@fastmail.fm>
2015-02-26 23:06:36 +0100

    [xmonad] Updates default layout.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -18,7 +18,19 @@ import System.IO
 myWorkspaces = [ "1:web", "2:chat", "3:browse", "4:media", "5:other" ]
 
 -- Layout
-myLayout = avoidStruts $ layoutHook defaultConfig
+myLayout = avoidStruts $ Full ||| tiled ||| Mirror tiled
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
 
 -- Windows management
 myManageHook = composeAll . concat $
@@ -36,7 +48,7 @@ myManageHook = composeAll . concat $
       viewShift          = doF . liftM2 (.) W.greedyView W.shift
       myClassWebShifts   = [ "Firefox", "Chromium" ]
       myTitleChatShifts  = [ "mail", "jabber" ]
-      myClassMediaShifts = [ "Vlc", ".spotify-wrapped" ]
+      myClassMediaShifts = [ "Vlc", "Spotify" ]
       myResourceIgnores  = [ "stalonetray" ]
 
 -- Define keys to add
