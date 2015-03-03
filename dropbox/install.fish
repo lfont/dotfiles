551ec2e4e9666cba3b9a5c8d5674e5924f7222d3
Loïc Fontaine <loicfontaine@fastmail.fm>
2015-03-03 17:12:06 +0100

    Removes all unused dotfiles.

--- dropbox/install.fish
+++
@@ -1,9 +0,0 @@
-. ./functions/install_archive.fish
-. ./functions/link_command.fish
-
-set install_dir (install_archive "https://www.dropbox.com/download?plat=lnx.x86_64" dropbox)
-ln -sf $install_dir ~/.dropbox-dist
-
-wget -O $DOTFILES_INSTALL_DIR/dropbox.py "https://www.dropbox.com/download?dl=packages/dropbox.py"
-link_command $DOTFILES_INSTALL_DIR/dropbox.py dropbox
-chmod +x $DOTFILES_INSTALL_DIR/dropbox.py
--- rust/install.fish
+++
@@ -1,1 +0,0 @@
-curl https://static.rust-lang.org/rustup.sh | sudo bash
