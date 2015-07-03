0a1cf18040b280b2bf39440f22c9a16989ffe468
Loïc Fontaine <loicfontaine@fastmail.fm>
2015-07-03 17:53:57 +0200

    [themes] Removes unused themes

--- themes/install.sh
+++ themes/install.sh
@@ -7,11 +7,6 @@ if [ ! -d ~/.themes/Numix ]; then
     ln -sf "$install_dir" ~/.themes/
 fi
 
-if [ ! -d ~/.themes/Color-UI ]; then
-    install_dir=$(f_install_repository https://github.com/UserContributer/Color-UI-theme-project.git Color-UI)
-    ln -sf "$install_dir/Color-UI" ~/.themes/
-fi
-
 
 mkdir -p ~/.icons
 
@@ -25,10 +20,4 @@ if [ ! -d ~/.icons/Numix-Circle ]; then
     install_dir=$(f_install_repository https://github.com/numixproject/numix-icon-theme-circle.git numix-icon-theme-circle)
     ln -sf "$install_dir"/Numix-Circle ~/.icons/
     gtk-update-icon-cache -f -t ~/.icons/Numix-Circle
-fi
-
-
-if [ ! -d $DOTFILES_INSTALL_DIR/slim-minimal ]; then
-    install_dir=$(f_install_repository https://github.com/naglis/slim-minimal.git slim-minimal)
-    sudo cp -r $install_dir /usr/share/slim/themes/
 fi
--- themes/update.sh
+++ themes/update.sh
@@ -1,11 +1,7 @@
 repos="$DOTFILES_INSTALL_DIR/Numix
-       $DOTFILES_INSTALL_DIR/Color-UI
        $DOTFILES_INSTALL_DIR/numix-icon-theme
-       $DOTFILES_INSTALL_DIR/numix-icon-theme-circle
-       $DOTFILES_INSTALL_DIR/slim-minimal"
+       $DOTFILES_INSTALL_DIR/numix-icon-theme-circle"
 
 for repo in $repos; do
     cd $repo && { git pull; cd -; }
 done
-
-sudo cp -r $DOTFILES_INSTALL_DIR/slim-minimal /usr/share/slim/themes/
