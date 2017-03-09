3f784f0ca4dc25516951127a39aec28c600b878e
Loïc Fontaine <loicfontaine@fastmail.fm>
2017-03-09 14:53:25 +0100

    [greg] Improve naming of podcasts.

--- greg/bin/sync-podcasts.sh
+++ greg/bin/sync-podcasts.sh
@@ -8,22 +8,22 @@ REMOTE_PODCASTS_LOG="$REMOTE_PODCASTS.log"
 
 if [ "$2" == "-u" ]
 then
-    # sync new podcasts
+    # download new podcasts
     greg sync
 fi
 
 # list of podcasts to sync
 echo -n > "$REMOTE_PODCASTS_LOG"
-cat "$REMOTE_PODCASTS" | while read podcast
+cat "$REMOTE_PODCASTS" | sort -V | while read podcast
 do
-  echo "$podcast" | sed -e 's,^.*$,'"$LOCAL_PATH"'/"&",' | xargs ls -Art | tail -n 3 | sed -e 's,^.*$,'"$podcast"'/&,' | sort >> "$REMOTE_PODCASTS_LOG"
+  echo "$podcast" | sed -e 's,^.*$,'"$LOCAL_PATH"'/"&",' | ls -1 | sort -Vr | tail -n 3 | sed -e 's,^.*$,'"$podcast"'/&,' >> "$REMOTE_PODCASTS_LOG"
 done
 
 # copy playlist
 cat "$REMOTE_PODCASTS_LOG" | sed -e 's,^.*$,Podcasts/&,' > "$REMOTE_PATH/_podcasts.m3u"
 
 # delete old files (--delete does not work with --files-from)
-diff <(cat "$REMOTE_PODCASTS_LOG") <(find "$REMOTE_PATH/Podcasts/" -type f | sed -e 's,'"$REMOTE_PATH"'/Podcasts/,,' | sort) | grep -e '^>' | cut -d' ' -f2- | while read file
+diff <(cat "$REMOTE_PODCASTS_LOG" | sort -V) <(find "$REMOTE_PATH/Podcasts/" -type f | sed -e 's,'"$REMOTE_PATH"'/Podcasts/,,' | sort -V) | grep -e '^>' | cut -d' ' -f2- | while read file
 do
   echo "deleting: $file"
   rm "$REMOTE_PATH/Podcasts/$file"
