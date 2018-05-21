2a70e02350e395045504eff9507973cf8f6939e6
Loïc Fontaine <loicfontaine@fastmail.fm>
2018-05-21 16:57:57 +0200

    [beets] Fix playlists.

--- beets/bin/sync-music.sh
+++ beets/bin/sync-music.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-REMOTE_HOST="$1"
-REMOTE_PATH=/media/seagate/Music
-REMOTE_PLAYLISTS_PATH=/media/seagate/Music/playlists
-
 LOCAL_PATH=~/Music
 LOCAL_PLAYLISTS_PATH=~/Music/playlists
 LOCAL_PLAYLISTS_TEMP_PATH=/tmp/playlists
+
+REMOTE_HOST="$1"
+REMOTE_PATH=/mnt/seagate/Music
+REMOTE_PLAYLISTS_PATH=/mnt/seagate/Music/playlists
 
 function fix_perms() {
   find $LOCAL_PATH/ -type d -name ".*" -print0 | xargs -0 rm -rf
@@ -14,32 +14,39 @@ function fix_perms() {
   find $LOCAL_PATH/ -type f -print0 | xargs -0 chmod 664
 }
 
-function download() {
-  rsync -a --partial --progress $REMOTE_HOST:$REMOTE_PATH/ $LOCAL_PATH/
-  fix_perms
-}
-
 if [ "$2" == "-d" ]
 then
-    download
-else
-  # sync music
+  echo "------------------------------------ PULL MUSIC"
+  rsync -a --partial --progress --exclude="/playlists/" $REMOTE_HOST:$REMOTE_PATH/ $LOCAL_PATH/
   fix_perms
-  rsync -a --partial --progress --delete --exclude="/Incoming/" $LOCAL_PATH/ $REMOTE_HOST:$REMOTE_PATH/
+else
+  echo "------------------------------------ PUSH MUSIC"
+  fix_perms
+  rsync -a --partial --progress --delete --exclude="/Incoming/" --exclude="/playlists/" $LOCAL_PATH/ $REMOTE_HOST:$REMOTE_PATH/
 
+  if [ "$2" == "-u" ]
+  then
+    echo "------------------------------------ UPDATE PLAYLISTS"
+    beet splupdate
+
+    # generate recently added playlist
+    beet list -p added:$(date +"%Y-%m-%d" --date="2 day ago").. | sed -e 's,'"$LOCAL_PATH"'/,,' > "$LOCAL_PLAYLISTS_PATH/_recently-added.m3u"
+  fi
+
+  echo "------------------------------------ PUSH PLAYLISTS"
   # fix music paths in playlists
   mkdir -p "$LOCAL_PLAYLISTS_TEMP_PATH"
-  rm -rf "$LOCAL_PLAYLISTS_TEMP_PATH/*"
+  rm -rf "$LOCAL_PLAYLISTS_TEMP_PATH"/*
   for playlist in "$LOCAL_PLAYLISTS_PATH"/*.m3u
   do
     cat "$playlist" | sed -e 's,^.*$,'"$REMOTE_PATH"'/&,' > "$LOCAL_PLAYLISTS_TEMP_PATH/$(basename $playlist)"
   done
 
-  # sync playlists
-  rsync -a --progress --delete "$LOCAL_PLAYLISTS_TEMP_PATH/" $REMOTE_HOST:"$REMOTE_PLAYLISTS_PATH/"
+  # sync
+  rsync -a --progress --delete --checksum "$LOCAL_PLAYLISTS_TEMP_PATH/" $REMOTE_HOST:"$REMOTE_PLAYLISTS_PATH/"
 
-  # triggrer library update (require API >= 1.15.0)
-  #SALT=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w12 | head -n1)
-  #TOKEN=$(pass show bibimbap/subsonic/$USER | head -n 1 | echo "$(cat -)$SALT" | md5sum | cut -d' ' -f1)
-  #curl "https://subsonic.$REMOTE_HOST/rest/startScan?u=$USER&t=$TOKEN&s=$SALT&v=1.15.0&c=sync-music.sh"
+  echo "------------------------------------ UPDATE DATABASE"
+  SALT=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w6 | head -n1 | tr '[:upper:]' '[:lower:]')
+  TOKEN=$(pass show bibimbap/subsonic/$USER | head -n 1 | echo -n "$(cat -)$SALT" | md5sum | cut -d' ' -f1)
+  curl "https://airsonic.$REMOTE_HOST/rest/startScan?u=$USER&t=$TOKEN&s=$SALT&v=1.15.0&c=sync-music.sh"
 fi
--- beets/bin/sync-playlists.sh
+++ beets/bin/sync-playlists.sh
@@ -1,42 +1,47 @@
 #!/usr/bin/env bash
 
 LOCAL_PATH="$HOME/Music"
-LOCAL_PLAYLISTS="$LOCAL_PATH/playlists"
+LOCAL_PLAYLISTS_PATH="$LOCAL_PATH/playlists"
+
 REMOTE_PATH="/media/$USER/$1"
-REMOTE_PLAYLISTS="$REMOTE_PATH/.playlists"
-REMOTE_PLAYLISTS_LOG="$REMOTE_PLAYLISTS.log"
+REMOTE_PLAYLISTS_PATH="$REMOTE_PATH/.playlists"
+REMOTE_PLAYLISTS_LOG_PATH="$REMOTE_PLAYLISTS_PATH.log"
 
 if [ "$2" == "-u" ]
 then
-    # update playlists
+    echo "------------------------------------ UPDATE PLAYLISTS"
     beet splupdate
 
     # generate recently added playlist
-    beet list -p added:$(date +"%Y-%m-%d" --date="2 day ago").. | sed -e 's,'"$LOCAL_PATH"'/,,' > "$LOCAL_PLAYLISTS/_recently-added.m3u"
+    beet list -p added:$(date +"%Y-%m-%d" --date="2 day ago").. | sed -e 's,'"$LOCAL_PATH"'/,,' > "$LOCAL_PLAYLISTS_PATH/_recently-added.m3u"
 fi
 
+echo "------------------------------------ COMPUTING FILES TO SYNC"
 # list of tracks to sync
-cat $(cat "$REMOTE_PLAYLISTS" | sed -e 's,^.*$,'"$LOCAL_PLAYLISTS"'/&.m3u,' | tr '\n' ' ') | sort -V | uniq > "$REMOTE_PLAYLISTS_LOG"
+cat $(cat "$REMOTE_PLAYLISTS_PATH" | sed -e 's,^.*$,'"$LOCAL_PLAYLISTS_PATH"'/&.m3u,' | tr '\n' ' ') | sort -V | uniq > "$REMOTE_PLAYLISTS_LOG_PATH"
 
 # copy playlists
-cat "$REMOTE_PLAYLISTS_LOG" | sed -e 's,^.*$,Music/&,' > "$REMOTE_PATH/_music.m3u"
+cat "$REMOTE_PLAYLISTS_LOG_PATH" | sed -e 's,^.*$,Music/&,' > "$REMOTE_PATH/_music.m3u"
 while read playlist
 do
-  cat "$LOCAL_PLAYLISTS/$playlist.m3u" | sed -e 's,^.*$,Music/&,' > "$REMOTE_PATH/$playlist.m3u"
-done < "$REMOTE_PLAYLISTS"
+  cat "$LOCAL_PLAYLISTS_PATH/$playlist.m3u" | sed -e 's,^.*$,Music/&,' > "$REMOTE_PATH/$playlist.m3u"
+done < "$REMOTE_PLAYLISTS_PATH"
 
 # append list of covers to sync
-echo -e "$(cat "$REMOTE_PLAYLISTS_LOG")\n$(cat "$REMOTE_PLAYLISTS_LOG" | cut -d/ -f-2 | sort -V | uniq | sed -e 's,^.*$,&/cover.jpg,')" | sort -V > "$REMOTE_PLAYLISTS_LOG.tmp"
-mv "$REMOTE_PLAYLISTS_LOG.tmp" "$REMOTE_PLAYLISTS_LOG"
+echo -e "$(cat "$REMOTE_PLAYLISTS_LOG_PATH")\n$(cat "$REMOTE_PLAYLISTS_LOG_PATH" | cut -d/ -f-2 | sort -V | uniq | sed -e 's,^.*$,&/cover.jpg,')" | sort -V > "$REMOTE_PLAYLISTS_LOG_PATH.tmp"
+mv "$REMOTE_PLAYLISTS_LOG_PATH.tmp" "$REMOTE_PLAYLISTS_LOG_PATH"
 
+echo "------------------------------------ DELETING UNNEEDED FILES"
 # delete old files (--delete does not work with --files-from)
-diff <(cat "$REMOTE_PLAYLISTS_LOG") <(find "$REMOTE_PATH/Music/" -type f | sed -e 's,'"$REMOTE_PATH"'/Music/,,' | sort -V) | grep -e '^>' | cut -d' ' -f2- | while read file
+diff <(cat "$REMOTE_PLAYLISTS_LOG_PATH") <(find "$REMOTE_PATH/Music/" -type f | sed -e 's,'"$REMOTE_PATH"'/Music/,,' | sort -V) | grep -e '^>' | cut -d' ' -f2- | while read file
 do
   echo "deleting: $file"
   rm "$REMOTE_PATH/Music/$file"
 done
 find "$REMOTE_PATH/Music/" -empty -delete
 
-# sync
-rsync -a --progress --modify-window=1 --files-from="$REMOTE_PLAYLISTS_LOG" "$LOCAL_PATH/" "$REMOTE_PATH/Music/"
+echo "------------------------------------ SYNC MUSIC"
+rsync -a --progress --modify-window=1 --files-from="$REMOTE_PLAYLISTS_LOG_PATH" "$LOCAL_PATH/" "$REMOTE_PATH/Music/"
+
+echo "------------------------------------ FREE SPACE"
 df -h "$REMOTE_PATH"
