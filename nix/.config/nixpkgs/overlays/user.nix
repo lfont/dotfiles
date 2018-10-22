b56d732246fa059ce67be7a8eabf228e3fa1b3e2
Loïc Fontaine <loicfontaine@fastmail.fm>
2018-10-22 20:33:07 +0200

    [nix] Upgrade to 18.09.
    - Install xmonad with nix.

--- nix/.config/nixpkgs/overlays/user.nix
+++ nix/.config/nixpkgs/overlays/user.nix
@@ -2,83 +2,101 @@
 # bootstrap with: nix-env -f '<nixpkgs>' -r -iA userPackages
 self: super: {
     userPackages = super.userPackages or {} // {
-        # helper script to keep user env clean
+        ## helper script to keep user env clean
         nix-rebuild = super.writeScriptBin "nix-rebuild"
         ''
             #!${super.stdenv.shell}
             exec nix-env -f '<nixpkgs>' -r -iA userPackages
         '';
-        # nix (nscd, https://github.com/NixOS/nix/issues/599)
+
+        ## nix (nscd, https://github.com/NixOS/nix/issues/599)
         nix = super.nix;
         nixops = super.nixops;
         glibcLocales = super.glibcLocales;
-        # common packages
+
+        ## common packages
         firefox = super.firefox;
         syncthing = super.syncthing;
         tree = super.tree;
         pass = super.pass;
-        # emacs
+	mosh = super.mosh;
+
+        ## wine
+	wine = super.wine;
+	winetricks = super.winetricks;
+
+        ## emacs
         emacs = super.emacs;
         xclip = super.xclip;
         multimarkdown = super.multimarkdown;
-        # themes (applied with dconf)
+
+        ## themes (applied with dconf)
         arc-theme = super.arc-theme;
         arc-icon-theme = super.arc-icon-theme;
-        paper-icon-theme = super.paper-icon-theme.overrideAttrs (oldAttrs: {
-            # https://github.com/NixOS/nixpkgs/issues/22652
-            postFixup =
-            ''
-                mkdir -p $out/share/icons/default
-                cat << EOF > $out/share/icons/default/index.theme
-                [Icon Theme]
-                Name=Default
-                Comment=Default Cursor Theme
-                Inherits=Paper
-                EOF
-            '' + oldAttrs.postFixup;
-        });
-        # i3
-        #i3 = super.i3;
-        #i3status = super.i3status;
-        #slock = super.slock; # need setuid
+        paper-icon-theme = super.paper-icon-theme;
+	# .overrideAttrs (oldAttrs: {
+        #     # https://github.com/NixOS/nixpkgs/issues/22652
+        #     postFixup =
+        #     ''
+        #         mkdir -p $out/share/icons/default
+        #         cat << EOF > $out/share/icons/default/index.theme
+        #         [Icon Theme]
+        #         Name=Default
+        #         Comment=Default Cursor Theme
+        #         Inherits=Paper
+        #         EOF
+        #     '' + oldAttrs.postFixup;
+        # });
+
+        ## wm
+        # i3 = super.i3;
+        # i3status = super.i3status;
+        # slock = super.slock; # need setuid
         dmenu = super.dmenu;
-        #nodejs = super.nodejs-6_x;
-        #xfce4_power_manager = super.xfce.xfce4_power_manager_gtk3; # https://forum.xfce.org/viewtopic.php?id=11190
+        # nodejs = super.nodejs-6_x;
+        # xfce4_power_manager = super.xfce.xfce4_power_manager_gtk3; # https://forum.xfce.org/viewtopic.php?id=11190
         gnome-settings-daemon = super.gnome3.gnome-settings-daemon;
         gnome-tweak-tool = super.gnome3.gnome-tweak-tool;
-        #polkit_gnome = super.polkit_gnome;
-        #wmctrl = super.wmctrl;
-        #dunst = super.dunst;
+        # polkit_gnome = super.polkit_gnome;
+        wmctrl = super.wmctrl;
+	stalonetray = super.stalonetray;
+        # dunst = super.dunst;
         feh = super.feh;
-        cwm = super.cwm.overrideAttrs (oldAttrs: {
-            name = "cwm-6.3";
-            src = super.fetchFromGitHub {
-                owner = "chneukirchen";
-                repo = "cwm";
-                rev = "a9dbac8209d0b5efa3bd5af0c287039545639117";
-                sha256 = "1m08gd6nscwfx6040zbg2zl89m4g73im68iflzcihd6pdc8rzzs4";
-            };
-        });
-        tint2 = super.tint2.overrideAttrs (oldAttrs: rec {
-            name = "tint2-${version}";
-            version = "16.4";
-            src = super.fetchFromGitLab {
-                owner = "o9000";
-                repo = "tint2";
-                rev = version;
-                sha256 = "1h9l45zimai2hqfcf2y98g4i03imhmvm3mlsld9x99i650kxr5jm";
-            };
-        });
+        # cwm = super.cwm.overrideAttrs (oldAttrs: {
+        #     name = "cwm-6.3";
+        #     src = super.fetchFromGitHub {
+        #         owner = "chneukirchen";
+        #         repo = "cwm";
+        #         rev = "a9dbac8209d0b5efa3bd5af0c287039545639117";
+        #         sha256 = "1m08gd6nscwfx6040zbg2zl89m4g73im68iflzcihd6pdc8rzzs4";
+        #     };
+        # });
+        # tint2 = super.tint2.overrideAttrs (oldAttrs: rec {
+        #     name = "tint2-${version}";
+        #     version = "16.4";
+        #     src = super.fetchFromGitLab {
+        #         owner = "o9000";
+        #         repo = "tint2";
+        #         rev = version;
+        #         sha256 = "1h9l45zimai2hqfcf2y98g4i03imhmvm3mlsld9x99i650kxr5jm";
+        #     };
+        # });
         ifstat = super.ifstat-legacy;
-        xdotool = super.xdotool;
+        # xdotool = super.xdotool;
         st = super.st;
-        # beets
+	xmonad = super.haskellPackages.ghcWithPackages(self: [
+	  self.xmonad self.xmonad-contrib self.xmonad-extras self.xmobar
+	]);
+
+        ## beets
         beets = super.beets;
         pygobject2 = super.python27Packages.pygobject2;
-        # pulseaudio
+
+        ## pulseaudio
         pavucontrol = super.pavucontrol;
         #pasystray = super.pasystray;
-        # daw
+
+        ## daw
         ardour = super.ardour;
         a2jmidid = super.a2jmidid;
         jack2 = super.jack2Full;
--- nix/.config/nixpkgs/overlays/work.nix
+++ nix/.config/nixpkgs/overlays/work.nix
@@ -18,32 +18,41 @@ self: super: {
         #         }
         #     ];
         # };
+	kafkacat = super.kafkacat;
+	postgresql = super.postgresql;
 
-        # Language specific tools
-        # In the future their will be specified at the project level (nix-shell)
-        # .NET
+        ## Language specific tools
+        ## In the future their will be specified at the project level (nix-shell)
+        ## .NET
         mono = super.mono;
         dotnet-sdk = super.dotnet-sdk;
-        # Elm
-        elm-make = super.elmPackages.elm-make;
-        elm-package = super.elmPackages.elm-package;
-        elm-reactor = super.elmPackages.elm-reactor;
-        elm-repl = super.elmPackages.elm-repl;
-        elm-format = super.elmPackages.elm-format.overrideAttrs (oldAttrs: {
-            version = "0.6.1-alpha";
-            src = super.fetchgit {
-                url = "http://github.com/avh4/elm-format";
-                sha256 = "0pypy8hfqn0vai5rwsidgcgf2n5k66gvgmp05p71r00xpj7gd2hg";
-                rev = "24cbc66245289dd3ca5c08a14e86358dc039fcf3";
-            };
-        });
-        elm-oracle = super.nodePackages.elm-oracle;
-        elm-test = super.nodePackages.elm-test;
-        # Haskell
+
+        ## Elm
+        # elm-make = super.elmPackages.elm-make;
+        # elm-package = super.elmPackages.elm-package;
+        # elm-reactor = super.elmPackages.elm-reactor;
+        # elm-repl = super.elmPackages.elm-repl;
+        elm-format = super.elmPackages.elm-format;
+	# .overrideAttrs (oldAttrs: {
+        #     version = "0.6.1-alpha";
+        #     src = super.fetchgit {
+        #         url = "http://github.com/avh4/elm-format";
+        #         sha256 = "0pypy8hfqn0vai5rwsidgcgf2n5k66gvgmp05p71r00xpj7gd2hg";
+        #         rev = "24cbc66245289dd3ca5c08a14e86358dc039fcf3";
+        #     };
+        # });
+        # elm-oracle = super.nodePackages.elm-oracle;
+        # elm-test = super.nodePackages.elm-test;
+
+        ## Haskell
         stack = super.stack; # broken in stack --docker
         hindent = super.haskellPackages.hindent;
-        # Node.js
+
+        ## Node.js
         nodejs = super.nodejs-6_x;
         tern = super.nodePackages.tern;
+
+        ## GO
+	go = super.go_1_10;
      };
 }
