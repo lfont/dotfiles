64069b788f32ed16d31b2c15b7f8526da9482db8
Loïc Fontaine <loicfontaine@fastmail.fm>
2019-02-19 15:52:30 +0100

    [X11] Rework HiDPI setup.

--- x11/.screenlayout/1.sh
+++ x11/.screenlayout/1.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-xrandr --output eDP-1 --auto --primary --dpi 220 \
+
+## HiDPI
+## https://wiki.archlinux.org/index.php/HiDPI
+## https://wiki.archlinux.org/index.php/xorg#Display_size_and_DPI
+xrandr --output eDP-1 --auto --primary --dpi 192 \
        --output DP-1 --off \
        --output DP-2 --off \
        --output HDMI-1 --off \
--- x11/.screenlayout/2.sh
+++ x11/.screenlayout/2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-xrandr --output eDP-1 --auto --dpi 200 \
+
+## HiDPI
+## https://wiki.archlinux.org/index.php/HiDPI
+## https://wiki.archlinux.org/index.php/xorg#Display_size_and_DPI
+xrandr --output eDP-1 --auto --dpi 192 \
        --output DP-1-2 --auto --primary --panning 3840x2160+3840+0 --scale 2x2 --right-of eDP-1 \
        --output DP-1 --off \
        --output DP-2 --off \
